03860f4d267a07dfc40da3ff105e6b511caa7f82
Max Barrett <mbarrett@rmn.com>
2013-11-12 14:25:41 +0000

    add uglify grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,26 @@
 module.exports = function(grunt) {
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-            files: ['public/js/*.js', 'public/js/plugins/*.js'],
-            options: {
-                //jshintrc: '.jshintrc'
-            }
-        }
-    });
- 
-    // Load the plugin that provides the "jshint" task.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
- 
-    // Default task(s).
-    grunt.registerTask('default', ['jshint']);
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			files: ['public/js/*.js', 'public/js/plugins/*.js'],
+			options: {
+				//jshintrc: '.jshintrc'
+			}
+		},
+		uglify: {
+			my_target: {
+				files: {
+					'dest/output.min.js': ['public/js/*.js']
+				}
+			}
+		}
+	});
+
+	// Load the plugin that provides the "jshint" task.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Default task(s).
+	grunt.registerTask('default', ['jshint', 'uglify']);
 };
